f9a15fbf64b8c364338c546ae4a9aba83288d459
Stephan Nikitin <nikitinste@gmail.com>
2019-10-01 20:26:32 +0300

    save progress

--- vm/game.c
+++ vm/game.c
@@ -12,12 +12,32 @@ static void	check_game(t_vm *vm)
 
 static void	cycle(t_vm *vm)
 {
-	//
+	t_carry	*cr;
+
+	cr = vm->carriages;
+	while (cr)
+	{
+		if (!cr->cycles_before)
+		{
+			cr->reg[0] = vm->area[cr->position];
+			if (vm->area[cr->position] >= 1 && vm->area[cr->position] <= 16)
+				cr->cycles_before = vm->cmd_prms[cr->reg[0]].cycles_before;
+		}
+		if (cr->cycles_before)
+			cr->cycles_before--;
+		if (!cr->cycles_before/* valid check! */)
+			vm->command[cr->reg[0] - 1](vm, cr);
+		/*else
+			move carreage to "calc" bytes count
+		*/
+		//
+		cr = cr->next;
+	}
 }
 
 void		game(t_vm *vm)
 {
-	while(vm->carriages || (vm->dump && vm->cycles_from_start < vm->dump))
+	while (vm->carriages || (vm->dump && vm->cycles_from_start < vm->dump))
 	{
 		cycle(vm);
 		if (vm->ctd_counter == vm->cycles_to_die)
@@ -29,6 +49,6 @@ void		game(t_vm *vm)
 	if (vm->dump && vm->carriages)
 		print_game_area();
 	else
-		ft_printf("Contestant %d, \"%s\", has won!\n", (int)vm->last_alive, \
+		ft_printf("Contestant %uhh, \"%s\", has won !\n", vm->last_alive, \
 			vm->processes[vm->last_alive - 1].cmp_name);
 }
