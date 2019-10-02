f172cded68d790c746910fcb28b54c01b64aa038
mvperepelitsyn <mvperepelitsyn@gmail.com>
2019-10-02 13:18:59 +0300

    my begininning on parser

--- vm/auxiliary.c
+++ vm/auxiliary.c
@@ -2,6 +2,15 @@
 
 int		display_usage()
 {
-	ft_printf("usage:\n\tthere should be an explanation\n");
+	ft_printf("usage:\n\there should be an explanation\n");
 	return (0);
+}
+
+void	ft_error(char *str)
+{
+	if (!str)
+		ft_putstr("ERROR\n");
+	else
+		ft_printf("%s\n", str);
+	exit(69);
 }
--- vm/parsing.c
+++ vm/parsing.c
@@ -1,6 +1,50 @@
 # include "vm.h"
 
-void	parsing(int argc, char **argv, t_vm	*vm)
+static void	check_num(t_process *champs, int num)
 {
-	//
+	t_process	*tmp;
+
+	if (num < 1 || num > MAX_PLAYERS)
+		ft_error("Wrong number of champion!\n");
+	if (champs == NULL)
+		return ;
+	tmp = champs;
+	while (tmp)
+	{
+		if (tmp->cmp_nbr == num)
+			ft_error("Wrong number of champion!\n");
+		tmp = tmp->next;
+	}
+}
+
+static void	parse_champ(int argc, char **argv, t_vm *vm)
+{
+	int			l;
+	int 		num;
+	t_process	*chmp;
+
+	l = 1;
+	chmp = vm->processes;
+	while (l <= argc)
+	{
+		if (ft_strequ(argv[l], "-n"))
+		{
+			if (!(chmp = (t_process *)ft_memalloc(sizeof(t_process))))
+				ft_error("Malloc couldn't allocate the memory\n");
+			l++;
+			chmp->cmp_nbr = ft_atoi(argv[l]);
+			l++;
+
+		}
+		l++;
+	}
+}
+
+void		parsing(int argc, char **argv, t_vm	*vm)
+{
+	if (argc > 15)
+		ft_error("Too many arguments");
+	else
+		parse_champ(argc, argv, vm);
+
 }
