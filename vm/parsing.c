b57e8c88771811bd1020698a492d0c32a9845cd4
mvperepelitsyn <mvperepelitsyn@gmail.com>
2019-10-02 19:07:33 +0300

    working on parsing 02.10.19

--- vm/parsing.c
+++ vm/parsing.c
@@ -1,41 +1,72 @@
-# include "vm.h"
+#include "vm.h"
+
+
 
 static void	check_num(t_process *champs, int num)
 {
-	t_process	*tmp;
+	int i;
 
+	i = 0;
 	if (num < 1 || num > MAX_PLAYERS)
-		ft_error("Wrong number of champion!\n");
-	if (champs == NULL)
-		return ;
-	tmp = champs;
-	while (tmp)
+		ft_error("Wrong number of champion file!\n");
+	while (i < MAX_PLAYERS)
 	{
-		if (tmp->cmp_nbr == num)
-			ft_error("Wrong number of champion!\n");
-		tmp = tmp->next;
+		if (champs[i].cmp_nbr == num)
+			ft_error("Wrong number of champion file!\n");
+		i++;
 	}
+}
+
+static void check_file_name(char *file_name, t_process *chmp, int num)
+{
+	int		i;
+	char 	*tmp;
+
+	tmp = NULL;
+	i = ft_strlen(file_name);
+	tmp = ft_strsub(file_name, i - 4, 4);
+	if (!ft_strequ(tmp, ".cor"))
+		ft_error("Wrong type of the champion file!\n");
+	ft_strdel(&tmp);
+	i = 0;
+	if (num != 0)
+		chmp->cmp_nbr = num;
+	else
+	{
+		while (i < MAX_PLAYERS)
+		{
+			if (chmp[i].cmp_nbr == 0)
+			i++;
+		}
+	}
+
+	fill_the_champ(chmp, file_name);
 }
 
 static void	parse_champ(int argc, char **argv, t_vm *vm)
 {
 	int			l;
-	int 		num;
-	t_process	*chmp;
+	long int 	num;
 
 	l = 1;
-	chmp = vm->processes;
 	while (l <= argc)
 	{
 		if (ft_strequ(argv[l], "-n"))
 		{
-			if (!(chmp = (t_process *)ft_memalloc(sizeof(t_process))))
-				ft_error("Malloc couldn't allocate the memory\n");
 			l++;
-			chmp->cmp_nbr = ft_atoi(argv[l]);
+			check_num(vm->processes, num = ft_atoi(argv[l]));
 			l++;
-
+			check_file_name(argv[l], vm->processes, num);
 		}
+		else if (ft_strequ(argv[l], "-dump"))
+		{
+			l++;
+			if ((num = ft_atoi(argv[l])) < 0 || num != (long long int)ft_latoi
+			(argv[l]))
+				ft_error("Invalid number after after flag -dump.\n");
+		}
+		else
+			check_file_name(argv[l], vm->processes, 0);
 		l++;
 	}
 }
@@ -46,5 +77,4 @@ void		parsing(int argc, char **argv, t_vm	*vm)
 		ft_error("Too many arguments");
 	else
 		parse_champ(argc, argv, vm);
-
 }
