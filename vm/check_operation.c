3c7cb211db06bd45a3f34f1aabf435ebda8f12b9
Stephan Nikitin <nikitinste@gmail.com>
2019-10-03 20:41:21 +0300

    save progress

--- vm/check_operation.c
+++ vm/check_operation.c
@@ -11,11 +11,29 @@ static int	check_reg(t_carry *cr, t_cycle *cycle)
 	return (1);
 }
 
-static int	check_params(t_carry *cr, t_cycle *cycle)
+static int	check_params(t_vm *vm, t_carry *cr, t_cycle *cycle)
 {
 	unsigned char	buf;
 
-	buf = g_cmd_prms[cr->cmd_code - 1].descript;
+	buf = vm->area[cr->position + 1];
+	if ((buf <<= 6) > 0)
+		return (0);
+	buf = vm->area[cr->position + 1];
+	buf <<= 4;
+	buf >>= 6;
+	cycle->descript[2] = buf;
+	buf = vm->area[cr->position + 1];
+	buf <<= 2;
+	buf >>= 6;
+	cycle->descript[1] = buf;
+	buf = vm->area[cr->position + 1];
+	buf >>= 6;
+	cycle->descript[0] = buf;
+	if ((g_cmd_prms.prms_count < 3 && cycle->descript[2]) || \
+			(g_cmd_prms.prms_count < 2 && cycle->descript[1]))
+		return (0);
+	// if ((g_cmd_prms.prm_types[0] & ) )
+	// 	;
 	return (1);
 }
 
@@ -25,7 +43,7 @@ int			check_operation(t_vm *vm, t_carry *cr, t_cycle *cycle)
 		return (0);
 	if (g_cmd_prms[cr->cmd_code - 1].descript)
 	{
-		if (!check_params(cr, cycle) || !check_reg(cr, cycle))
+		if (!check_params(vm, cr, cycle) || !check_reg(cr, cycle))
 		{
 			calc_shift(cr);
 			return (0);
