e3d5b0a0a69a31665393ecf276c65546794501ae
Stephan Nikitin <nikitinste@gmail.com>
2019-10-18 14:52:40 +0300

    wrong descript byte bug fixed

--- vm/commands.c
+++ vm/commands.c
@@ -14,10 +14,10 @@ void	live(t_carry *cr)
 	if ((champ_nbr[0] * -1) > 0 \
 		&& (champ_nbr[0] * -1) <= (int)cr->vm->champs_count)
 	{
-		cr->vm->last_alive = dir + 1;
-		cr->last_champ = dir + 1;
-		cr->last_alive_cycle = cr->vm->cycles_from_start;
-		cr->vm->last_alive_cycle = cr->vm->cycles_from_start;
+		cr->vm->last_alive = dir;
+		cr->last_champ = dir;
+		cr->last_alive_cycle = cr->vm->cycles_from_start + 1;
+		cr->vm->last_alive_cycle = cr->vm->cycles_from_start + 1;
 	}
 	if (cr->vm->v)
 		cr->vm->back[cr->position] |= 200;
